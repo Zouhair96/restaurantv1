77842c6dc722f42a9ccf1bcb8ca96d9e3405ae20
Zouhair96 <zouhair.benali96@gmail.com>
2026-01-13 18:01:31 +0100

    chage db url

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // HARDCODED CONNECTION STRING FOR DEMO
 // Note: In a real production app, this should be an Environment Variable.
-const connectionString = "postgresql://neondb_owner:npg_1rpb6OTNdnDR@ep-solitary-meadow-aebg6bqq-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require";
+const connectionString = "postgresql://neondb_owner:npg_K7WXZeLJY0Sv@ep-old-fire-ae5lg5tk-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require";
 
 const pool = new Pool({
     connectionString,
