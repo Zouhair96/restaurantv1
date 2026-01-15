f63394897409c4f81d8872f20483fc97c494f34a
Zouhair96 <zouhair.benali96@gmail.com>
2026-01-15 02:53:56 +0100

    fining bugs

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+      '/api': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   }
 })
