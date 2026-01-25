931025e580078a9cf86b8813212b7a80c27d7faa
Zouhair96 <zouhair.benali96@gmail.com>
2026-01-25 17:04:15 +0100

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,9 +38,19 @@ const Home = () => {
             className: 'w-40 sm:w-56 md:w-64 lg:w-72 top-10 left-4 sm:left-10 md:top-20 md:left-20 animate-float',
         },
         {
+            src: '/menu_phone_mockup.png',
+            alt: 'Smartphone displaying restaurant menu',
+            className: 'w-32 sm:w-40 md:w-52 lg:w-60 top-1/4 right-8 sm:right-12 md:right-20 animate-float',
+        },
+        {
             src: 'https://b.zmtcdn.com/data/o2_assets/b4f62434088b0ddfa9b370991f58ca601743060218.png',
             alt: 'A bamboo steamer with dumplings',
             className: 'w-28 sm:w-36 md:w-48 top-10 right-4 sm:right-10 md:top-16 md:right-16 animate-float',
+        },
+        {
+            src: '/qr_code_floating.png',
+            alt: 'QR code for restaurant menu',
+            className: 'w-24 sm:w-32 md:w-40 bottom-1/4 left-8 sm:left-16 md:left-24 animate-float',
         },
         {
             src: 'https://b.zmtcdn.com/data/o2_assets/316495f4ba2a9c9d9aa97fed9fe61cf71743059024.png',
@@ -48,9 +58,14 @@ const Home = () => {
             className: 'w-32 sm:w-40 md:w-56 bottom-8 right-5 sm:right-10 md:bottom-16 md:right-20 animate-float',
         },
         {
+            src: '/hand_phone_menu.png',
+            alt: 'Hand holding phone with menu',
+            className: 'w-36 sm:w-44 md:w-56 lg:w-64 bottom-12 left-12 sm:left-20 md:bottom-20 md:left-28 animate-float',
+        },
+        {
             src: 'https://b.zmtcdn.com/data/o2_assets/70b50e1a48a82437bfa2bed925b862701742892555.png',
             alt: 'A basil leaf',
-            className: 'w-8 sm:w-12 top-1/4 left-1/3 animate-float',
+            className: 'w-8 sm:w-12 top-1/3 left-1/3 animate-float',
         },
         {
             src: 'https://b.zmtcdn.com/data/o2_assets/9ef1cc6ecf1d92798507ffad71e9492d1742892584.png',
@@ -60,7 +75,7 @@ const Home = () => {
         {
             src: 'https://b.zmtcdn.com/data/o2_assets/9ef1cc6ecf1d92798507ffad71e9492d1742892584.png',
             alt: 'A slice of tomato',
-            className: 'w-8 sm:w-10 top-3/4 left-1/4 animate-float',
+            className: 'w-8 sm:w-10 top-2/3 left-1/4 animate-float',
         },
     ];
 
